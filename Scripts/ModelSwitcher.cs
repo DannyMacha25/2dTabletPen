a38521a30ae0bc3462cf702d07e237cd50a6d556
Danny <=>
2024-01-02 12:01:23 -0500

    Ui buttons made and populate the scroll views

--- Scripts/ModelSwitcher.cs
+++ Scripts/ModelSwitcher.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
+using Unity.XR.CoreUtils;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModelSwitcher : MonoBehaviour
 {
@@ -24,10 +28,17 @@ public class ModelSwitcher : MonoBehaviour
 
 
     // TODO: For easier compatability, make it so a controller can call this rather than have keycodes hard coded in here
-    // TODO: Adding button through script https://docs.unity3d.com/2018.3/Documentation/ScriptReference/UI.Button-onClick.html
+    // TODO: Either make each model a prefab with drawable properties, or add those properties through script
+
+
+    [SerializeField] private GameObject modelScrollContentHolder; // The content object under the scroll view hierarchy
+    [SerializeField] private GameObject modelSelectionButtonPrefab;
+    [SerializeField] private GameObject monthScrollContentHolder; // The content object under the scroll view hierarchy
 
     private string parentPath = "Models/"; // Models/{name}/{month}/{name}
     private string jsonPath = "ModelInfo";
+
+    private GameObject currentLoadedModel;
 
     private GameObject currentModel;
     // Start is called before the first frame update
@@ -35,6 +46,10 @@ public class ModelSwitcher : MonoBehaviour
     {
         // Load the JSON
         ModelInfo info = LoadModelInfoFromJSON(jsonPath);
+
+        Debug.Log(info.models[0].name);
+
+        PopulateModelNameMenu(info);
 
         GameObject[] models = Resources.LoadAll<GameObject>("Models/");
         //GameObject cafe = Resources.Load<GameObject>("Models/cafe/September/{name}");
@@ -51,6 +66,11 @@ public class ModelSwitcher : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Creates a ModelInfo object with the resource path of a json file
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>ModelInfo (null if error)</returns>
     private ModelInfo LoadModelInfoFromJSON(string path)
     {
         TextAsset jsonTextAsset = Resources.Load<TextAsset>(path);
@@ -60,6 +80,7 @@ public class ModelSwitcher : MonoBehaviour
             jsonString = jsonTextAsset.text;
         } else
         {
+            Debug.LogError("[Model Loader] JSON file not found");
             return null;
         }
 
@@ -68,26 +89,103 @@ public class ModelSwitcher : MonoBehaviour
         return info;
     }
 
-    // TODO: Populate a menu with all of the models and their names
-    private void PopulateModelNameMenu()
+    /// <summary>
+    /// Populate the model scroll view given a ModelInfo object
+    /// </summary>
+    /// <param name="modelinfo"></param>
+    private void PopulateModelNameMenu(ModelInfo modelinfo)
     {
+        foreach(Model m in modelinfo.models)
+        {
+            // Instantiate button and set parent as content in scroll view
+            var btn = Instantiate(modelSelectionButtonPrefab);
+            btn.transform.SetParent(modelScrollContentHolder.transform, false);
 
+            // Add PopulateModelDateMenu function to each button 
+            btn.GetComponent<Button>().onClick.AddListener(delegate { PopulateModelDateMenu(m); });
+            List<GameObject> btn_children = new List<GameObject>();
+            btn.GetChildGameObjects(btn_children);
+
+            // Change Button name to the month
+            TextMeshProUGUI tmp = btn_children[0].GetComponent<TextMeshProUGUI>();
+         
+            if(tmp != null)
+            {
+                tmp.SetText(m.name);
+            }
+        }
     }
 
-    // TODO: Whenever a model is clicked in the model menu, populate this menu with the possible dates,
-    //  possibly take the name, maybe the index in the model info array
-    public void PopulateModelDateMenu(string name)
+    /// <summary>
+    /// Populate the month scroll view given a Model object
+    /// </summary>
+    /// <param name="m"></param>
+    public void PopulateModelDateMenu(Model m)
     {
+        // Clear the other buttons first
+        List<GameObject> monthButtons = new List<GameObject>();
 
+        monthScrollContentHolder.GetChildGameObjects(monthButtons);
+
+        if(monthButtons.Count > 0)
+        {
+            foreach(GameObject btn in monthButtons)
+            {
+                Destroy(btn);
+            }
+        }
+
+        foreach(string month in m.months)
+        {
+            // Instantiate button and set parent as content in scroll view
+            var btn = Instantiate(modelSelectionButtonPrefab);
+            btn.transform.SetParent(monthScrollContentHolder.transform, false);
+
+            // Add LoadModel function to each button 
+            btn.GetComponent<Button>().onClick.AddListener(delegate { LoadModel(m.name, month); }); 
+            List<GameObject> btn_children = new List<GameObject>();
+            btn.GetChildGameObjects(btn_children);
+
+            // Change Button name to the month
+            TextMeshProUGUI tmp = btn_children[0].GetComponent<TextMeshProUGUI>();
+
+            if (tmp != null)
+            {
+                tmp.SetText(month);
+            }
+        }
     }
 
-    // TODO: Load and instantiate model into the scene given the name and month
-    //  Probably unload the current model too.
+    /// <summary>
+    /// Loads a model from assets given the month and the name
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="month"></param>
     public void LoadModel(string name, string month)
     {
+        // Destroys currently loaded model
+        if(currentLoadedModel != null)
+        {
+            Destroy(currentLoadedModel);
+            currentLoadedModel = null;
+        }
+
+        string path = String.Format("Models/{0}/{1}/{0}", name, month);
+        GameObject obj = Resources.Load<GameObject>(path);
+
+        if(obj != null)
+        {
+            currentLoadedModel = Instantiate(obj);
+        } else
+        {
+            Debug.LogError(String.Format("The model path was null {0}", path));
+        }
+
 
     }
 
+
+    public void TestFunction(string msg) { Debug.Log(msg); }
     
 }
 
@@ -98,7 +196,7 @@ class ModelInfo
 }
 
 [Serializable]
-struct Model
+public struct Model
 {
     public string name;
     public List<string> months;
